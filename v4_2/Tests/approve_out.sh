f9527e77044c2efcc2f05da2b3b338aa3b599572
djdarland <student@dennisdarland.com>
2020-07-05 19:07:04 -0500

    Added some files I had missed for last commit.

---
+++ v4_2/Tests/approve_out.sh
@@ -0,0 +1,17 @@
+if test -f OUT/$1.out.tmp
+then
+cp OUT/$1.out.tmp REFOUT/$1.refout
+else
+rm REFOUT/$1.refout
+touch REFOUT/$1.refout
+fi
+if test -f ERR/$1.err.tmp
+then
+cp ERR/$1.err.tmp REFERR/$1.referr
+else
+rm REFERR/$1.referr
+touch REFERR/$1.referr
+fi
+echo "$1 approved" >APPROVED/$1.approved
+echo "$1 approved"
+
